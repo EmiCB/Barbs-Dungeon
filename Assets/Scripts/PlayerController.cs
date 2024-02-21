24ca8e9fbd29fb20f7f29e09924b2ae521b7bde3
Emi Brown <emicb55@gmail.com>
2024-02-20 21:18:29 -0600

    Add health system and temporary bar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
     // Movement variables
     private float moveSpeed = 10.0f;
     private Vector2 movementInput = Vector2.zero;
+
+    // Health system
+    private int maxHealth = 10; // TODO: split out into a character stats ScriptableObject
+    private HealthSystem healthSystem;
+    public HealthBar healthBar;
 
     // Component references
     private Rigidbody2D rb;
@@ -12,6 +17,9 @@ public class PlayerMovement : MonoBehaviour {
     // Initialize player
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+
+        // initialize stats
+        healthSystem = new HealthSystem(maxHealth, healthBar);
     }
 
     // Run physics
@@ -25,5 +33,14 @@ public class PlayerMovement : MonoBehaviour {
     /// <param name="value">The action value that contains the Vector2 X/Y input from the player's input device.</param>
     void OnMove(InputValue value) {
         movementInput = value.Get<Vector2>();
+    }
+
+    // Using skill buttons as temp tests for health system
+    void OnSkill1() {
+        healthSystem.ApplyDamage(1);
+    }
+
+    void OnSkill2() {
+        healthSystem.ApplyHeal(1);
     }
 }
