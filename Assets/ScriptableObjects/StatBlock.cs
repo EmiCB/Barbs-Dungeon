d15178c98bd5ad9c3b71394d383c1ffba4d120bf
Emi Brown <emicb55@gmail.com>
2024-03-26 22:17:01 -0500

    Add stats to stats to statblock

--- Assets/ScriptableObjects/StatBlock.cs
+++ Assets/ScriptableObjects/StatBlock.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "StatBlock", menuName = "ScriptableObjects/StatBlock", order = 1)]
 public class StatBlock : ScriptableObject {
-    // Resouces
+    [Space(5), Header("Resources"), Space(5)]
     public int baseHealth;
     public int baseMana;
     public int baseStamina;
 
-    // Movement
+    [Space(5), Header("Movement"), Space(5)]
     public float baseMoveSpeed;
     public float acceleration;
     public float deceleration;
     public float rollCooldown;
 
     // General RPG stats
-    // TODO: fill in
+    [Space(5), Header("RPG Stats"), Space(5)]
+    public int defense;             // reduces incoming damage
+    public int strength;            // determines melee damage
+    public int dexterity;           // influences ranged damage, accuracy, critical hit rates
+    public int lethality;           // amplifies critical hit damage
+    public int intelligence;        // increases spell power
+    public int wisdom;              // affects mana regen rate
+    public int agility;             // depletes when run / dodge, slowly recharges
 }
